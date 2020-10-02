6cf3adf90b63278e4b8e557410c29cefd2a1879f
roboter <robby.roboter@gmail.com>
2020-10-03 00:22:38 +0300

    Change name

--- src/app/core/components/header-bar-brand.component.ts
+++ src/app/core/components/header-bar-brand.component.ts
@@ -13,9 +13,9 @@ import { Component } from '@angular/core';
         <i class="fab js-logo fa-angular fa-2x" aria-hidden="true"></i>
       </a>
       <a class="navbar-item nav-home" router-link="/">
-        <span class="brand-first">MY</span>
-        <span class="brand-second">SHOPPING</span>
-        <span class="brand-third">LIST</span>
+        <span class="brand-first">GOT</span>
+        <span class="brand-second">STL</span>
+        <span class="brand-third">.COM</span>
       </a>
     </div>
   `,
