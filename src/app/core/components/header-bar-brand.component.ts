1524c5ebedfefe179793d07461fe3a856c8e4fa5
Aleksei Prokopov <aleksei.prokopov@novater.com>
2021-01-31 15:35:25 +0200

    no angular logo

--- src/app/core/components/header-bar-brand.component.ts
+++ src/app/core/components/header-bar-brand.component.ts
@@ -4,14 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header-bar-brand',
   template: `
     <div class="navbar-brand">
-      <a
-        class="navbar-item"
-        href="https://angular.io/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i class="fab js-logo fa-angular fa-2x" aria-hidden="true"></i>
-      </a>
       <a class="navbar-item nav-home" router-link="/">
         <span class="brand-first">GOT</span>
         <span class="brand-second">STL</span>
