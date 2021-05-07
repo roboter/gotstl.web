b8648b460a80a109a4983d14dc534a4a72241d92
Aleksei Prokopov <aleksei.prokopov@novater.com>
2021-05-07 14:37:45 +0300

    update about

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="content-container">
       <div class="content-title-group not-found">
-        <h2 class="title">Product Wish List</h2>
+        <h2 class="title">GotSTL</h2>
         <p>
           This project was created to help represent a fundamental app written
           with Angular. The shopping theme is used throughout the app.
@@ -14,9 +14,7 @@ import { Component } from '@angular/core';
         <h2 class="title">Resources</h2>
         <ul>
           <li>
-            <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
-              Code in GitHub
-            </a>
+            OpenJSCAD
           </li>
         </ul>
       </div>
